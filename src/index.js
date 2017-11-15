fd305d6c5cf9fca0048eae243f93b5b8c18f135b
Dod-Annie <936166900@qq.com>
2017-11-15 19:13:00 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+import { createStore } from 'redux'
+
+function counter(state = 0, action) {
+  switch (action.type) {
+    case '加机关枪':
+      return state + 1
+    case '减机关枪':
+      return state - 1
+    default:
+      return 10
+  }
+}
+
+const store = createStore(counter)
+
+const init = store.getState()
+console.log(init)
+
+function listener(){
+  const current = store.getState()
+  console.log(`现在有机枪${current}把`)
+}
+store.subscribe(listener)
+
+
+store.dispatch({type:'加机关枪'})
+store.dispatch({type:'减机关枪'})
