7ac604967c677975fecf7402f1858d66e543b71e
Dod-Annie <936166900@qq.com>
2017-11-15 19:51:52 +0800

    add

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const store = this.props.store
+    const num = store.getState()
+    const addGUN = this.props.addGUN
+    const removeGUN = this.props.removeGUN
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>现在有{num}把枪</h1>
+        <button onClick={() => store.dispatch(addGUN())}> 申请武器</button>
+        <button onClick={() => store.dispatch(removeGUN())}> 上交武器</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux'
-
-function counter(state = 0, action) {
-  switch (action.type) {
-    case '加机关枪':
-      return state + 1
-    case '减机关枪':
-      return state - 1
-    default:
-      return 10
-  }
-}
+import { counter, addGUN, removeGUN } from './index.redux'
 
 const store = createStore(counter)
-
-const init = store.getState()
-console.log(init)
-
-function listener(){
-  const current = store.getState()
-  console.log(`现在有机枪${current}把`)
+function render() {
+  ReactDOM.render(<App store={store} addGUN={addGUN} removeGUN={removeGUN}/>, document.getElementById('root'))
 }
-store.subscribe(listener)
+registerServiceWorker();
+
+render()
+
+store.subscribe(render)
+// import { createStore } from 'redux'
+
+// function counter(state = 0, action) {
+//   switch (action.type) {
+//     case '加机关枪':
+//       return state + 1
+//     case '减机关枪':
+//       return state - 1
+//     default:
+//       return 10
+//   }
+// }
+
+// const store = createStore(counter)
+
+// const init = store.getState()
+// console.log(init)
+
+// function listener(){
+//   const current = store.getState()
+//   console.log(`现在有机枪${current}把`)
+// }
+// store.subscribe(listener)
 
 
-store.dispatch({type:'加机关枪'})
-store.dispatch({type:'减机关枪'})
+// store.dispatch({type:'加机关枪'})
+// store.dispatch({type:'减机关枪'})
